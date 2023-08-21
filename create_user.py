509d7e178027d7ad4fcca2716c4aeff2276c06ce
Brandon Wallace <brandonwallace403@gmail.com>
2023-08-20 23:28:50 -0400

    added class review, movies and director

--- create_user.py
+++ create_user.py
@@ -1,17 +1,44 @@
 from models import User,Session,engine
 
-mysession= Session(bind=engine)
+def main():
+    session = Session()
+
+    # Create an author and their books
+    author = Author(name="J.K. Rowling")
+    book1 = Book(title="Harry Potter and the Sorcerer's Stone", author=author)
+    book2 = Book(title="Harry Potter and the Chamber of Secrets", author=author)
+    session.add(author)
+    session.commit()
+
+    # Create reviews for a book
+    review1 = Review(content="A magical adventure!", book=book1)
+    review2 = Review(content="Even better than the first!", book=book1)
+    session.add_all([review1, review2])
+    session.commit()
+
+    # Query and print data
+    selected_author = session.query(Author).first()
+    print(f"Author: {selected_author.name}")
+    for book in selected_author.books:
+        print(f"Book: {book.title}")
+        for review in book.reviews:
+            print(f"  Review: {review.content}")
+
+if __name__ == "__main__":
+    main()
+
+# mysession= Session(bind=engine)
 
 
-new_user = User(username='Alice', email='alice@example.com')
-new_user1 = User(username='Bob', email='bob@example.com')
+# new_user = User(username='Alice', email='alice@example.com')
+# new_user1 = User(username='Bob', email='bob@example.com')
 
 
-mysession.add(new_user)
-mysession.add(new_user1)
+# mysession.add(new_user)
+# mysession.add(new_user1)
 
 
-mysession.commit()
+# mysession.commit()
 
 
 # new_user = User(username='brandon nimmo', email='john@example.com')
--- models.py
+++ models.py
@@ -1,26 +1,42 @@
+# models.py
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
-engine = create_engine("sqlite:///models.db", echo=True) 
-Session = sessionmaker(bind=engine)
-session = Session()
-
-
-
-class User(Base):
-    __tablename__ = 'users'
+class Director(Base):
+    __tablename__ = 'directors'
 
     id = Column(Integer, primary_key=True)
-    username = Column(String, unique=False)
-    email = Column(String)
+    name = Column(String)
+    movies = relationship("Movie", backref="director", lazy=True)
 
-def __repr__(self):
-        return f"User {self.id}: "\
-             + f"{self.username} "\
-             + f"email: {self.email}\n"
+class Movie(Base):
+    __tablename__ = 'movies'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    director_id = Column(Integer, ForeignKey('directors.id'))
+    reviews = relationship("Review", backref="movie", lazy=True)
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer, primary_key=True)
+    rating = Column(Integer)
+    comment = Column(String)
+    movie_id = Column(Integer, ForeignKey('movies.id'))
+
+# Create an engine and session
+engine = create_engine('sqlite:///movies.db', echo=True)
+Session = sessionmaker(bind=engine)
+
+
+# def __repr__(self):
+#         return f"User {self.id}: "\
+#              + f"{self.username} "\
+#              + f"email: {self.email}\n"
 
 
 
